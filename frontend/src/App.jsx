71d15531438a13e7df26fb1fef4fc90728366807
Samarjit25 <samarjitroy025@gmail.com>
2025-07-03 18:39:59 +0530

    minor

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,6 +16,7 @@ const App = () => {
     <Router>
     <Routes>
       <Route path="/" element={<Home/>}/>
+      
       <Route path="/success" element={<Success/>}/>
       <Route path="*" element={<NotFound/>}/>
     </Routes>
